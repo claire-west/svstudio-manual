ba2755867ac584eb5fb1bfecf4648174944a9265
cwest <bokthaku@gmail.com>
2023-01-19 21:05:23 -0500

    Use localized revision timing

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 apt install mkdocs
 apt-get install libcairo2-dev libfreetype6-dev libffi-dev libjpeg-dev libpng-dev libz-dev
-pip install mkdocs mkdocs-material mkdocs-material-extensions mkdocs-git-revision-date-plugin mkdocs-glightbox pillow cairosvg
+pip install mkdocs mkdocs-material mkdocs-material-extensions mkdocs-git-revision-date-localized-plugin mkdocs-glightbox pillow cairosvg
